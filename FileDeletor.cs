52ec3dba638b48dcac8154d0c5d9fdac972f53a9
Kanishka Upadhyay <61813125+kanishkaupadhyay@users.noreply.github.com>
2024-01-03 15:23:17 +0530

    Uploaded Via Desktop

---
+++ FileDeletor.cs
@@ -0,0 +1,38 @@
+﻿namespace FileDeletor
+{
+    public class FileDeletor
+    {
+        public static bool DeleteFilesFromPath(string path)
+        {
+            bool success = false;
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                    if (files != null)
+                    {
+                        Console.WriteLine($"Number of files to delete: ");
+                        
+                        FileInfo fileInfo;
+                        foreach (string filePath in files)
+                        {
+                            fileInfo = new FileInfo(filePath);
+                            Console.WriteLine($"File Information");
+                            Console.WriteLine($"File Name: {fileInfo.Name}, File Extension: {fileInfo.Extension}, File Size: {fileInfo.Length}, File Creation Date: {fileInfo.CreationTimeUtc.Date}");
+                            Console.WriteLine("Deleting File: " + filePath);
+                            File.Delete(filePath);
+                        }
+                        success = true;
+                    }
+                    Console.WriteLine("No files found under this folder.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return success;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Press A to Start\nPress Q to Exit");
+    var keyPressed = Console.ReadKey(true);
+    switch (keyPressed.Key)
+    {
+        case ConsoleKey.A:
+            DeleteFiles();
+            break;
+        case ConsoleKey.Q:
+            Console.WriteLine("Exiting the application \n");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Invalid key pressed \n");
+            continue;
+    }
+}
+
+static void DeleteFiles()
+{
+    Console.WriteLine("Please enter the file path");
+    string? path = Console.ReadLine();
+    if (!string.IsNullOrEmpty(path))
+    {
+        if (Directory.Exists(path))
+        {
+            FileDeletor.FileDeletor.DeleteFilesFromPath(path);
+        }
+        else
+        {
+            Console.WriteLine("\nThe given directory does not exist");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid path given please try again.");
+    }
+}
+
